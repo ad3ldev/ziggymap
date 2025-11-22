fcc9faab573eab5c309fdcf73dbd2e8717f0732c
AbdelRahman Adel AbdelFatttah <ad3ldev@gmail.com>
2025-11-22 20:07:06 +0200

    fix: small typo

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 const options = @import("./helpers/options.zig");
-const ziggyMap = @import("./structs/Ziggymap.zig").ZiggyMap;
+const ziggymap = @import("./structs/Ziggymap.zig").ZiggyMap;
 
 pub fn main() !void {
     const args = options.parseArgs() catch |err| {
         std.debug.print("Error: {any}\n", .{err});
         return;
     };
-    const ziggy_map = ziggyMap.init(args);
+    const ziggy_map = ziggymap.init(args);
     std.debug.print("{any}\n", .{ziggy_map});
 }
