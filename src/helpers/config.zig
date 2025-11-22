d6f3b8c21350fef3cc35f9a54ae5b24870c93902
AbdelRahman Adel AbdelFatttah <ad3ldev@gmail.com>
2025-11-22 15:39:48 +0200

    feat: colors and fix of utils

--- src/helpers/config.zig
+++ src/helpers/config.zig
@@ -1,43 +1,55 @@
 const Config = struct {
     language: []u8 = "en",
-    // TODO:
-    // style_file
-    initial_zoom: ?u8 = null,
-    max_zoom: u8 = 18,
-    zoom_step: u8 = 0.2,
 
-    // TODO:
-    // initial_latitude
-    // initial_longitude
+    /// TODO: use the new one
+    /// source: []u8 = "https://api.maptiler.com",
+    source: []u8 = "http://mapscii.me/",
+
+    style_file: []u8 = "../styles/dark.json",
+
+    initial_zoom: ?u8 = null,
+    max_zoom: u64 = 18,
+    zoom_step: f64 = 0.2,
+
+    initial_latitude: f64 = 52.51298,
+    initial_longitude: f64 = 13.42012,
+
     simplify_polylines: bool = false,
+
     use_braille: bool = true,
 
-    // TODO:
-    // source
     persist_downloaded_tiles: bool = true,
-    tile_range: u8 = 14,
-    project_size: u8 = 256,
-    label_margin: u8 = 5,
 
-    layers: struct { house_number_label: struct {
-        margin: u8 = 4,
-    }, poi_label: struct {
-        cluster: bool = true,
-        margin: u8 = 5,
-    }, place_label: struct {
-        cluster: bool = true,
-    }, state_label: struct {
-        cluster: bool = true,
-    } },
+    tile_range: u64 = 14,
+    project_size: u64 = 256,
+
+    label_margin: u64 = 5,
+
+    layers: .{
+        .house_number_label = .{
+            .margin = 4,
+        },
+        .poi_label = .{
+            .cluster = true,
+            .margin = 5,
+        },
+        .place_label = .{
+            .cluster = true,
+        },
+        .state_label = .{
+            .cluster = true,
+        },
+    },
 
     // TODO:
     // input
     // output
     //
     headless: bool = false,
+
     delimeter: []u8 = "\n\r",
+
     poi_marker: u8 = "*",
 };
 
 pub const config: Config = Config;
-
--- src/helpers/utils.zig
+++ src/helpers/utils.zig
@@ -3,7 +3,7 @@ const constants = @import("./constants.zig");
 const math = @import("std").math;
 const std = @import("std");
 
-pub fn clamp(num: u8, min: u8, max: u8) u8 {
+pub fn clamp(comptime T: type, num: T, min: T, max: T) T {
     if (num <= min) {
         return min;
     }
@@ -13,37 +13,59 @@ pub fn clamp(num: u8, min: u8, max: u8) u8 {
     return num;
 }
 
-pub fn base_zoom(zoom: f16) u8 {
-    return @min(config.tile_range, @max(0.0, @floor(zoom)));
+pub fn baseZoom(zoom: f64) f64 {
+    const a = @max(0.0, @floor(zoom));
+    const b = @min(config.tile_range, a);
+    return b;
 }
 
-pub fn tile_size_at_zoom(zoom: f16) u8 {
-    return config.project_size * math.pow(2, zoom - base_zoom(zoom));
+pub fn tileSizeAtZoom(zoom: f64) f64 {
+    const a = math.pow(f64, 2, zoom - baseZoom(zoom));
+    const b = config.project_size * a;
+    return b;
 }
 
-pub fn deg_to_rad(angle: f16) f16 {
-    return (angle / 180) * math.pi;
+pub fn degToRad(angle: f64) f64 {
+    const a = angle / 180;
+    const b = a * math.pi;
+    return b;
 }
 
-pub fn long_lat_to_tile(long: f16, lat: f16, zoom: f16) struct { x: f16, y: f16, z: f16 } {
-    return struct {
-        x: f16 = (long + 180) / 360 * math.pow(2, zoom),
-        y: f16 = (1 - math.log(10, math.tan(lat * math.pi / 180) + 1 / math.cos(lat * math.pi / 180)) / math.pi) / 2 * math.pow(2, zoom),
-        z: f16 = zoom,
+pub fn longLat2Title(long: f64, lat: f64, zoom: f64) struct { x: f64, y: f64, z: f64 } {
+    const xA = long + 180;
+    const xB = xA / 360;
+    const xC = xB * math.pow(f64, 2, zoom);
+
+    const yA = lat * math.pi / 180.0;
+    const yB = math.tan(yA) + 1 / math.cos(yA);
+    const yC = (1 - math.log(f64, 10, yB) / math.pi) / 2;
+    const yD = yC * math.pow(f64, 2.0, zoom);
+
+    return .{
+        .x = xC,
+        .y = yD,
+        .z = zoom,
     };
 }
 
-pub fn tile_to_long_lat(x: f16, y: f16, zoom: f16) struct { long: f16, lat: f16 } {
-    const n: f16 = math.pi - 2 * math.pi * y / math.pow(2, zoom);
+pub fn tileToLongLat(x: f64, y: f64, zoom: f64) struct { long: f64, lat: f64 } {
+    const n: f64 = math.pi - 2 * math.pi * y / math.pow(2, zoom);
 
-    return struct {
-        lon: f16 = x / math.pow(2, zoom) * 360 - 180,
-        lat: f16 = 180 / math.pi * math.atan(0.5 * (math.exp(n) - math.exp(-n))),
-    };
+    const longA = x / math.pow(2, zoom) * 360 - 180;
+
+    const latA = math.exp(n) - math.exp(-n);
+    const latB = math.atan(0.5 * latA);
+    const latC = 180 / math.pi * latB;
+    return .{ .long = longA, .lat = latC };
 }
 
-pub fn meters_per_pixel(zoom: f16, lat: f16) f16 {
-    return (math.cos(lat * math.PI / 180) * 2 * math.PI * constants.RADIUS) / (256 * math.pow(2, zoom));
+pub fn metersPerPixel(zoom: f64, lat: f64) f64 {
+    const a = math.pow(f64, 2, zoom);
+    const b = 256 * a;
+    const c = math.cos(lat * math.pi / 180);
+    const d = c * 2 * math.pi * constants.RADIUS;
+    const e = (d) / b;
+    return e;
 }
 
 pub fn hex_to_rgb(color: [7]u8) struct { r: u8, g: u8, b: u8 } {
@@ -51,35 +73,40 @@ pub fn hex_to_rgb(color: [7]u8) struct { r: u8, g: u8, b: u8 } {
     const g_hex = color[3..5];
     const b_hex = color[5..7];
 
-    return struct {
-        r: u8 = std.fmt.parseInt(r_hex, 16),
-        g: u8 = std.fmt.parseInt(g_hex, 16),
-        b: u8 = std.fmt.parseInt(b_hex, 16),
+    return .{
+        .r = std.fmt.parseInt(r_hex, 16),
+        .g = std.fmt.parseInt(g_hex, 16),
+        .b = std.fmt.parseInt(b_hex, 16),
     };
 }
 
-pub fn fn_digits(number: f16, digits: u8) u8 {
-    return math.floor(number * math.pow(10, digits)) / math.pow(10, digits);
+pub fn toDigits(number: f64, digits: u64) u64 {
+    const a = math.pow(u64, 10, digits);
+    const b = math.pow(u64, 10, digits);
+    const c = math.floor(number * a) / b;
+    return c;
 }
 
-pub fn normalize(ll: struct { lat: f16, long: f16 }) struct { lat: f16, long: f16 } {
+pub fn normalize(ll: struct { lat: f64, long: f64 }) struct { lat: f64, long: f64 } {
     if (ll.long < -180) {
         ll.long += 360;
     }
     if (ll.long > 180) {
         ll.lon -= 360;
     }
+
     if (ll.lat > 85.0511) {
         ll.lat = 85.0511;
     }
     if (ll.lat < -85.0511) {
         ll.lat = -85.0511;
     }
+
     return ll;
 }
 
-pub fn population(val: u8) u8 {
-    var bits = 0;
+pub fn population(val: u64) u64 {
+    var bits: u64 = 0;
     while (val > 0) {
         bits += val & 1;
         val >>= 1;
